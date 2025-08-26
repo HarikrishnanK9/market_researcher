e7cb0325ae24eb7c9199240025af96b0d086ad07
Harikrishnan K <128063333+HarikrishnanK9@users.noreply.github.com>
2025-08-26 13:23:16 +0530

    Update main.py

--- src/market_researcher/main.py
+++ src/market_researcher/main.py
@@ -23,19 +23,19 @@ def run():
         raise Exception(f"An error occurred while running the crew: {e}")
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    {
-        'topic': 'Electric Vehicles',
-        'input_text': """
-                    According to recent industry data, the global electric vehicle (EV) market saw a 35% year-over-year growth in 2024, driven largely by surging demand in Asia and Europe.
-                    China accounted for over 60% of global EV sales, while European countries saw a 20% increase due to new government subsidies and stricter emission policies.
-                    Major automakers are accelerating their EV lineup, with Ford, GM, and Volkswagen announcing over $50 billion in combined investment for battery innovation by 2026.
-                    Additionally, consumers are increasingly prioritising range efficiency and fast-charging capabilities when selecting EV models.
-        """
-        }
+# def train():
+#     """
+#     Train the crew for a given number of iterations.
+#     """
+#     {
+#         'topic': 'Electric Vehicles',
+#         'input_text': """
+#                     According to recent industry data, the global electric vehicle (EV) market saw a 35% year-over-year growth in 2024, driven largely by surging demand in Asia and Europe.
+#                     China accounted for over 60% of global EV sales, while European countries saw a 20% increase due to new government subsidies and stricter emission policies.
+#                     Major automakers are accelerating their EV lineup, with Ford, GM, and Volkswagen announcing over $50 billion in combined investment for battery innovation by 2026.
+#                     Additionally, consumers are increasingly prioritising range efficiency and fast-charging capabilities when selecting EV models.
+#         """
+#         }
 
 
     try:
